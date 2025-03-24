fb58bcecc183ac3a862177bb1cf7596196da7632
zax <zax@jobui.com>
2025-03-24 11:38:26 +0800

    修改菜单栏

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -3,9 +3,17 @@ import { sidebar } from "vuepress-theme-hope";
 // 精选图标：https://vuepress-theme-hope.github.io/v2/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 export default sidebar([
   {
-    text: "code",
+    text: "php",
     icon: "",
-    prefix: "/code/",
+    prefix: "/php/",
+    link: "",
+    collapsable: true,
+    children: "structure",
+  },
+  {
+    text: "python",
+    icon: "",
+    prefix: "/python/",
     link: "",
     collapsable: true,
     children: "structure",
